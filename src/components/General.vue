6032c33e0960b718403b086e279a4f861a4044ed
student <student@GFL-1541.ad.geeksforless.net>
2017-10-13 21:00:11 +0300

    bs

---
+++ src/components/General.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="general">
+    client
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'general',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
